e261c09d99f69c99c8e49c7ae458a224a695e6ef
mortvest <dmitry.serykh@gmail.com>
2022-01-19 19:49:35 +0100

    release v0.1.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ STL_MODULE_BASE = CFFI_BASE + "build_stl_"
 LOESS_MODULE_BASE = CFFI_BASE + "build_loess_"
 ENV_VAR = "HASTL_BACKENDS"
 
-VERSION = "0.1.4"
+VERSION = "0.1.5"
 
 
 def check_backend(backend_str, base):
